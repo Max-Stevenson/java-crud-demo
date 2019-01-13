2ba9a31a33a5673a29f34c617dc56753950dbfcf
Max Stevenson <maxstevenson@msn.com>
2019-01-13 12:37:14 +0000

    eighth commit: - Added controller routes for retrieving an employee by Id and adding a new employee.

--- src/main/java/com/makers/cruddemo/controller/EmployeeRestController.java
+++ src/main/java/com/makers/cruddemo/controller/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.makers.cruddemo.controller;
 import com.makers.cruddemo.entity.Employee;
 import com.makers.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,25 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeID}")
+    public  Employee getEmployee(@PathVariable int employeeID) {
+
+        Employee theEmployee = employeeService.findByID(employeeID);
+
+        if(theEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeID);
+        }
+        return theEmployee;
+    }
+
+    @PostMapping("/employees/")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+        theEmployee.setId(0);
+
+        employeeService.save(theEmployee);
+
+        return theEmployee;
     }
 }
